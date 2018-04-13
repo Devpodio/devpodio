panjiesw/theia

Theia hosted plugin runner. Draft.
Signed-off-by: Mykola Morhun <mmorhun@redhat.com>

packages/plugin/src/browser/hosted-plugin-client.ts
@@ -1,0 +1,96 @@
+/*
+ * Copyright (C) 2015-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+import { injectable, inject } from 'inversify';
+import URI from '@theia/core/lib/common/uri';
+import { MessageService } from '@theia/core/lib/common';
+import { LabelProvider } from '@theia/core/lib/browser';
+import { WindowService } from '@theia/core/lib/browser/window/window-service';
+import { WorkspaceService } from '@theia/workspace/lib/browser';
+import { FileSystem } from '@theia/filesystem/lib/common';
+import { FileDialogFactory, DirNode } from '@theia/filesystem/lib/browser';
+import { HostedPluginServer } from '../common/plugin-protocol';
+import { HostedPluginCommands } from './plugin-api-frontend-contribution';
+
+@injectable()
+export class HostedPluginClient {
+    @inject(HostedPluginServer)
+    protected readonly hostedPluginServer: HostedPluginServer;
+    @inject(MessageService)
+    protected readonly messageService: MessageService;
+    @inject(FileDialogFactory)
+    protected readonly fileDialogFactory: FileDialogFactory;
+    @inject(LabelProvider)
+    protected readonly labelProvider: LabelProvider;
+    @inject(WindowService)
+    protected readonly windowService: WindowService;
+    @inject(FileSystem)
+    protected readonly fileSystem: FileSystem;
+    @inject(WorkspaceService)
+    protected readonly workspaceService: WorkspaceService;
+
+    protected pluginLocation: URI | undefined;
+    protected pluginInstanceUri: string | undefined;
+
+    run() {
+        if (this.pluginLocation) {
+            this.doRunRequest(this.pluginLocation);
+        } else {
+            this.selectPluginPath().then(() => {
+                if (this.pluginLocation) {
+                    this.doRunRequest(this.pluginLocation);
+                }
+            });
+        }
+    }
+
+    terminate() {
+        this.hostedPluginServer.terminateHostedPluginInstance().then(() => {
+            this.messageService.info((this.pluginInstanceUri ? this.pluginInstanceUri : 'The instance') + ' has been terminated.');
+        }).catch(error => {
+            this.messageService.warn(this.getErrorMessage(error));
+        });
+    }
+
+    async selectPluginPath() {
+        const root = await this.workspaceService.root || await this.fileSystem.getCurrentUserHome();
+        const rootUri = new URI(root.uri);
+        const rootStat = await this.fileSystem.getFileStat(rootUri.toString());
+        const name = this.labelProvider.getName(rootUri);
+        const label = await this.labelProvider.getIcon(root);
+        const rootNode = DirNode.createRoot(rootStat, name, label);
+        const dialog = this.fileDialogFactory({ title: HostedPluginCommands.SELECT_PLUGIN_PATH.label! });
+        dialog.model.navigateTo(rootNode);
+        const node = await dialog.open();
+        if (node) {
+            if (await this.hostedPluginServer.isValidPlugin(node.uri.toString())) {
+                this.pluginLocation = node.uri;
+                this.messageService.info('Plugin folder is set to: ' + node.uri.toString());
+            } else {
+                this.messageService.error('Specified folder does not contain valid plugin.');
+            }
+        }
+    }
+
+    protected doRunRequest(pluginLocation: URI): void {
+        this.messageService.info('Starting hosted instancse server ...');
+        this.hostedPluginServer.runHostedPlugin(pluginLocation.toString()).then(uri => {
+            this.pluginInstanceUri = uri;
+            this.windowService.openNewWindow(uri.toString());
+            this.messageService.info('Hosted instancse is running at: ' + uri);
+        }).catch(error => {
+            this.messageService.error('Failed to run hosted plugin instanse: ' + this.getErrorMessage(error));
+        });
+    }
+
+    protected getErrorMessage(error: Error): string {
+        return error.message.substring(error.message.lastIndexOf(':') + 1);
+    }
+}

packages/plugin/src/common/plugin-protocol.ts
@@ -28,4 +28,10 @@ export const HostedPluginServer = Symbol('HostedPluginServer');
 export interface HostedPluginServer extends JsonRpcServer<HostedPluginClient> {
     getHostedPlugin(): Promise<Plugin | undefined>;
     onMessage(message: string): Promise<void>;
+
+    isValidPlugin(uri: string): Promise<boolean>;
+    runHostedPlugin(uri: string): Promise<string>;
+    terminateHostedPluginInstance(): Promise<void>;
+    isHostedPluginRunning(): Promise<boolean>;
+    getHostedPluginInstanceURI(): Promise<string>;
 }

packages/plugin/src/node/plugin-service.ts
@@ -9,11 +9,13 @@
  *   Red Hat, Inc. - initial API and implementation
  */
 import * as express from 'express';
+import URI from '@theia/core/lib/common/uri';
 import { injectable, inject } from "inversify";
 import { BackendApplicationContribution } from '@theia/core/lib/node/backend-application';
 import { HostedPluginServer, HostedPluginClient, Plugin } from '../common/plugin-protocol';
 import { HostedPluginReader } from './plugin-reader';
 import { HostedPluginSupport } from './hosted-plugin';
+import { HostedPluginRunner } from './hosted-plugin-runner';
 
 const pluginPath = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + './theia/plugins/';
 
@@ -30,8 +32,10 @@ export class PluginApiContribution implements BackendApplicationContribution {
 @injectable()
 export class HostedPluginServerImpl implements HostedPluginServer {
 
-    constructor(@inject(HostedPluginReader) private readonly reader: HostedPluginReader,
-        @inject(HostedPluginSupport) private readonly hostedPlugin: HostedPluginSupport) {
+    constructor(
+        @inject(HostedPluginReader) private readonly reader: HostedPluginReader,
+        @inject(HostedPluginSupport) private readonly hostedPlugin: HostedPluginSupport,
+        @inject(HostedPluginRunner) protected readonly hostedPluginRunner: HostedPluginRunner) {
     }
 
     dispose(): void {
@@ -52,4 +56,33 @@ export class HostedPluginServerImpl implements HostedPluginServer {
         this.hostedPlugin.onMessage(message);
         return Promise.resolve();
     }
+
+    isValidPlugin(uri: string): Promise<boolean> {
+        return Promise.resolve(this.hostedPluginRunner.isValidPlugin(new URI(uri)));
+    }
+
+    runHostedPlugin(uri: string): Promise<string> {
+        return this.uriToStrPromise(this.hostedPluginRunner.run(new URI(uri)));
+    }
+
+    terminateHostedPluginInstance(): Promise<void> {
+        return Promise.resolve(this.hostedPluginRunner.terminate());
+    }
+
+    isHostedPluginRunning(): Promise<boolean> {
+        return Promise.resolve(this.hostedPluginRunner.isRunning());
+    }
+
+    getHostedPluginInstanceURI(): Promise<string> {
+        return Promise.resolve(this.hostedPluginRunner.getInstanceURI().toString());
+    }
+
+    protected uriToStrPromise(promise: Promise<URI>): Promise<string> {
+        return new Promise((resolve, reject) => {
+            promise.then((uri: URI) => {
+                resolve(uri.toString());
+            }).catch(error => reject(error));
+        });
+    }
+
 }

Devpodio/devpodio

packages/plugin/src/browser/hosted-plugin-client.ts
@@ -1,0 +1,99 @@
+/*
+ * Copyright (C) 2015-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+import { injectable, inject } from 'inversify';
+import URI from '@theia/core/lib/common/uri';
+import { MessageService } from '@theia/core/lib/common';
+import { LabelProvider, isNative } from '@theia/core/lib/browser';
+import { WindowService } from '@theia/core/lib/browser/window/window-service';
+import { WorkspaceService } from '@theia/workspace/lib/browser';
+import { FileSystem } from '@theia/filesystem/lib/common';
+import { FileDialogFactory, DirNode } from '@theia/filesystem/lib/browser';
+import { HostedPluginServer } from '../common/plugin-protocol';
+import { HostedPluginCommands } from './plugin-api-frontend-contribution';
+
+@injectable()
+export class HostedPluginClient {
+    @inject(HostedPluginServer)
+    protected readonly hostedPluginServer: HostedPluginServer;
+    @inject(MessageService)
+    protected readonly messageService: MessageService;
+    @inject(FileDialogFactory)
+    protected readonly fileDialogFactory: FileDialogFactory;
+    @inject(LabelProvider)
+    protected readonly labelProvider: LabelProvider;
+    @inject(WindowService)
+    protected readonly windowService: WindowService;
+    @inject(FileSystem)
+    protected readonly fileSystem: FileSystem;
+    @inject(WorkspaceService)
+    protected readonly workspaceService: WorkspaceService;
+
+    protected pluginLocation: URI | undefined;
+    protected pluginInstanceUri: string | undefined;
+
+    run() {
+        if (this.pluginLocation) {
+            this.doRunRequest(this.pluginLocation);
+        } else {
+            this.selectPluginPath().then(() => {
+                if (this.pluginLocation) {
+                    this.doRunRequest(this.pluginLocation);
+                }
+            });
+        }
+    }
+
+    terminate() {
+        this.hostedPluginServer.terminateHostedPluginInstance().then(() => {
+            this.messageService.info((this.pluginInstanceUri ? this.pluginInstanceUri : 'The instance') + ' has been terminated.');
+        }).catch(error => {
+            this.messageService.warn(this.getErrorMessage(error));
+        });
+    }
+
+    async selectPluginPath() {
+        const root = await this.workspaceService.root || await this.fileSystem.getCurrentUserHome();
+        const rootUri = new URI(root.uri);
+        const rootStat = await this.fileSystem.getFileStat(rootUri.toString());
+        const name = this.labelProvider.getName(rootUri);
+        const label = await this.labelProvider.getIcon(root);
+        const rootNode = DirNode.createRoot(rootStat, name, label);
+        const dialog = this.fileDialogFactory({ title: HostedPluginCommands.SELECT_PLUGIN_PATH.label! });
+        dialog.model.navigateTo(rootNode);
+        const node = await dialog.open();
+        if (node) {
+            if (await this.hostedPluginServer.isPluginValid(node.uri.toString())) {
+                this.pluginLocation = node.uri;
+                this.messageService.info('Plugin folder is set to: ' + node.uri.toString());
+            } else {
+                this.messageService.error('Specified folder does not contain valid plugin.');
+            }
+        }
+    }
+
+    protected doRunRequest(pluginLocation: URI): void {
+        this.messageService.info('Starting hosted instancse server ...');
+        this.hostedPluginServer.runHostedPlugin(pluginLocation.toString()).then(uri => {
+            this.pluginInstanceUri = uri;
+            if (!isNative) {
+                // Open a new tab in case of browser
+                this.windowService.openNewWindow(uri.toString());
+            }
+            this.messageService.info('Hosted instancse is running at: ' + uri);
+        }).catch(error => {
+            this.messageService.error('Failed to run hosted plugin instanse: ' + this.getErrorMessage(error));
+        });
+    }
+
+    protected getErrorMessage(error: Error): string {
+        return error.message.substring(error.message.lastIndexOf(':') + 1);
+    }
+}

packages/plugin/src/common/plugin-protocol.ts
@@ -28,4 +28,10 @@ export const HostedPluginServer = Symbol('HostedPluginServer');
 export interface HostedPluginServer extends JsonRpcServer<HostedPluginClient> {
     getHostedPlugin(): Promise<Plugin | undefined>;
     onMessage(message: string): Promise<void>;
+
+    isPluginValid(uri: string): Promise<boolean>;
+    runHostedPlugin(uri: string): Promise<string>;
+    terminateHostedPluginInstance(): Promise<void>;
+    isHostedPluginRunning(): Promise<boolean>;
+    getHostedPluginInstanceURI(): Promise<string>;
 }

packages/plugin/src/node/plugin-service.ts
@@ -9,11 +9,13 @@
  *   Red Hat, Inc. - initial API and implementation
  */
 import * as express from 'express';
+import URI from '@theia/core/lib/common/uri';
 import { injectable, inject } from "inversify";
 import { BackendApplicationContribution } from '@theia/core/lib/node/backend-application';
 import { HostedPluginServer, HostedPluginClient, Plugin } from '../common/plugin-protocol';
 import { HostedPluginReader } from './plugin-reader';
 import { HostedPluginSupport } from './hosted-plugin';
+import { HostedPluginRunner } from './hosted-plugin-runner';
 
 const pluginPath = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + './theia/plugins/';
 
@@ -30,8 +32,10 @@ export class PluginApiContribution implements BackendApplicationContribution {
 @injectable()
 export class HostedPluginServerImpl implements HostedPluginServer {
 
-    constructor(@inject(HostedPluginReader) private readonly reader: HostedPluginReader,
-        @inject(HostedPluginSupport) private readonly hostedPlugin: HostedPluginSupport) {
+    constructor(
+        @inject(HostedPluginReader) private readonly reader: HostedPluginReader,
+        @inject(HostedPluginSupport) private readonly hostedPlugin: HostedPluginSupport,
+        @inject(HostedPluginRunner) protected readonly hostedPluginRunner: HostedPluginRunner) {
     }
 
     dispose(): void {
@@ -52,4 +56,33 @@ export class HostedPluginServerImpl implements HostedPluginServer {
         this.hostedPlugin.onMessage(message);
         return Promise.resolve();
     }
+
+    isPluginValid(uri: string): Promise<boolean> {
+        return Promise.resolve(this.hostedPluginRunner.isPluginValid(new URI(uri)));
+    }
+
+    runHostedPlugin(uri: string): Promise<string> {
+        return this.uriToStrPromise(this.hostedPluginRunner.run(new URI(uri)));
+    }
+
+    terminateHostedPluginInstance(): Promise<void> {
+        return Promise.resolve(this.hostedPluginRunner.terminate());
+    }
+
+    isHostedPluginRunning(): Promise<boolean> {
+        return Promise.resolve(this.hostedPluginRunner.isRunning());
+    }
+
+    getHostedPluginInstanceURI(): Promise<string> {
+        return Promise.resolve(this.hostedPluginRunner.getInstanceURI().toString());
+    }
+
+    protected uriToStrPromise(promise: Promise<URI>): Promise<string> {
+        return new Promise((resolve, reject) => {
+            promise.then((uri: URI) => {
+                resolve(uri.toString());
+            }).catch(error => reject(error));
+        });
+    }
+
 }