pablodonayre/theia

support console when contexts
Signed-off-by: Anton Kosyakov <anton.kosyakov@typefox.io>

packages/console/src/browser/console-widget.ts
@@ -17,6 +17,7 @@
 import { ElementExt } from '@phosphor/domutils';
 import { injectable, inject, postConstruct, interfaces, Container } from 'inversify';
 import { TreeSourceNode } from '@theia/core/lib/browser/source-tree';
+import { ContextKey } from '@theia/core/lib/browser/context-key-service';
 import { BaseWidget, PanelLayout, Widget, Message, MessageLoop, StatefulWidget, CompositeTreeNode } from '@theia/core/lib/browser';
 import { MonacoEditor } from '@theia/monaco/lib/browser/monaco-editor';
 import URI from '@theia/core/lib/common/uri';
@@ -38,6 +39,7 @@ export interface ConsoleOptions {
         uri: URI
         options?: MonacoEditor.IOptions
     }
+    inputFocusContextKey?: ContextKey<boolean>
 }
 
 @injectable()
@@ -84,7 +86,7 @@ export class ConsoleWidget extends BaseWidget implements StatefulWidget {
 
     @postConstruct()
     protected async init(): Promise<void> {
-        const { id, title } = this.options;
+        const { id, title, inputFocusContextKey } = this.options;
         const { label, iconClass, caption } = Object.assign({}, title);
         this.id = id;
         this.title.closable = true;
@@ -109,6 +111,9 @@ export class ConsoleWidget extends BaseWidget implements StatefulWidget {
         this.toDispose.push(input.getControl().onDidLayoutChange(() => this.resizeContent()));
         this.toDispose.push(input.getControl().onDidChangeConfiguration(({ fontInfo }) => fontInfo && this.updateFont()));
         this.updateFont();
+        if (inputFocusContextKey) {
+            this.toDispose.push(input.onFocusChanged(() => inputFocusContextKey.set(this.hasInputFocus())));
+        }
     }
 
     protected createInput(node: HTMLElement): Promise<MonacoEditor> {
@@ -247,6 +252,10 @@ export class ConsoleWidget extends BaseWidget implements StatefulWidget {
             // tslint:disable-next-line:no-any
             this.input.restoreViewState((<any>oldState)['input']);
         }
+    }
+
+    hasInputFocus(): boolean {
+        return this._input && this._input.isFocused({ strict: true });
     }
 
 }

packages/debug/src/browser/console/debug-console-contribution.ts
@@ -16,8 +16,12 @@
 
 import { interfaces, injectable } from 'inversify';
 import { AbstractViewContribution, bindViewContribution, WidgetFactory } from '@theia/core/lib/browser';
+import { ContextKeyService, ContextKey } from '@theia/core/lib/browser/context-key-service';
 import { ConsoleWidget, ConsoleOptions } from '@theia/console/lib/browser/console-widget';
 import { DebugConsoleSession } from './debug-console-session';
+
+export type InDebugReplContextKey = ContextKey<boolean>;
+export const InDebugReplContextKey = Symbol('inDebugReplContextKey');
 
 @injectable()
 export class DebugConsoleContribution extends AbstractViewContribution<ConsoleWidget> {
@@ -50,13 +54,20 @@ export class DebugConsoleContribution extends AbstractViewContribution<ConsoleWi
     };
 
     static create(parent: interfaces.Container): ConsoleWidget {
-        const child = ConsoleWidget.createContainer(parent, DebugConsoleContribution.options);
+        const inputFocusContextKey = parent.get<InDebugReplContextKey>(InDebugReplContextKey);
+        const child = ConsoleWidget.createContainer(parent, {
+            ...DebugConsoleContribution.options,
+            inputFocusContextKey
+        });
         const widget = child.get(ConsoleWidget);
         widget.session = child.get(DebugConsoleSession);
         return widget;
     }
 
     static bindContribution(bind: interfaces.Bind): void {
+        bind(InDebugReplContextKey).toDynamicValue(({ container }) =>
+            container.get(ContextKeyService).createKey('inDebugRepl', false)
+        ).inSingletonScope();
         bind(DebugConsoleSession).toSelf().inSingletonScope();
         bindViewContribution(bind, DebugConsoleContribution).onActivation((context, _) => {
             // eagerly initialize the debug console session

Devpodio/devpodio

packages/console/src/browser/console-widget.ts
@@ -17,6 +17,7 @@
 import { ElementExt } from '@phosphor/domutils';
 import { injectable, inject, postConstruct, interfaces, Container } from 'inversify';
 import { TreeSourceNode } from '@devpodio/core/lib/browser/source-tree';
+import { ContextKey } from '@devpodio/core/lib/browser/context-key-service';
 import { BaseWidget, PanelLayout, Widget, Message, MessageLoop, StatefulWidget, CompositeTreeNode } from '@devpodio/core/lib/browser';
 import { MonacoEditor } from '@devpodio/monaco/lib/browser/monaco-editor';
 import URI from '@devpodio/core/lib/common/uri';
@@ -38,6 +39,7 @@ export interface ConsoleOptions {
         uri: URI
         options?: MonacoEditor.IOptions
     }
+    inputFocusContextKey?: ContextKey<boolean>
 }
 
 @injectable()
@@ -84,7 +86,7 @@ export class ConsoleWidget extends BaseWidget implements StatefulWidget {
 
     @postConstruct()
     protected async init(): Promise<void> {
-        const { id, title } = this.options;
+        const { id, title, inputFocusContextKey } = this.options;
         const { label, iconClass, caption } = Object.assign({}, title);
         this.id = id;
         this.title.closable = true;
@@ -109,6 +111,9 @@ export class ConsoleWidget extends BaseWidget implements StatefulWidget {
         this.toDispose.push(input.getControl().onDidLayoutChange(() => this.resizeContent()));
         this.toDispose.push(input.getControl().onDidChangeConfiguration(({ fontInfo }) => fontInfo && this.updateFont()));
         this.updateFont();
+        if (inputFocusContextKey) {
+            this.toDispose.push(input.onFocusChanged(() => inputFocusContextKey.set(this.hasInputFocus())));
+        }
     }
 
     protected createInput(node: HTMLElement): Promise<MonacoEditor> {
@@ -247,6 +252,10 @@ export class ConsoleWidget extends BaseWidget implements StatefulWidget {
             // tslint:disable-next-line:no-any
             this.input.restoreViewState((<any>oldState)['input']);
         }
+    }
+
+    hasInputFocus(): boolean {
+        return this._input && this._input.isFocused({ strict: true });
     }
 
 }

packages/debug/src/browser/console/debug-console-contribution.ts
@@ -16,8 +16,12 @@
 
 import { interfaces, injectable } from 'inversify';
 import { AbstractViewContribution, bindViewContribution, WidgetFactory } from '@devpodio/core/lib/browser';
+import { ContextKeyService, ContextKey } from '@devpodio/core/lib/browser/context-key-service';
 import { ConsoleWidget, ConsoleOptions } from '@devpodio/console/lib/browser/console-widget';
 import { DebugConsoleSession } from './debug-console-session';
+
+export type InDebugReplContextKey = ContextKey<boolean>;
+export const InDebugReplContextKey = Symbol('inDebugReplContextKey');
 
 @injectable()
 export class DebugConsoleContribution extends AbstractViewContribution<ConsoleWidget> {
@@ -50,13 +54,20 @@ export class DebugConsoleContribution extends AbstractViewContribution<ConsoleWi
     };
 
     static create(parent: interfaces.Container): ConsoleWidget {
-        const child = ConsoleWidget.createContainer(parent, DebugConsoleContribution.options);
+        const inputFocusContextKey = parent.get<InDebugReplContextKey>(InDebugReplContextKey);
+        const child = ConsoleWidget.createContainer(parent, {
+            ...DebugConsoleContribution.options,
+            inputFocusContextKey
+        });
         const widget = child.get(ConsoleWidget);
         widget.session = child.get(DebugConsoleSession);
         return widget;
     }
 
     static bindContribution(bind: interfaces.Bind): void {
+        bind(InDebugReplContextKey).toDynamicValue(({ container }) =>
+            container.get(ContextKeyService).createKey('inDebugRepl', false)
+        ).inSingletonScope();
         bind(DebugConsoleSession).toSelf().inSingletonScope();
         bindViewContribution(bind, DebugConsoleContribution).onActivation((context, _) => {
             // eagerly initialize the debug console session