pablodonayre/theia

support resource when closure
Signed-off-by: Anton Kosyakov <anton.kosyakov@typefox.io>

packages/editor/src/browser/editor-contribution.ts
@@ -17,9 +17,11 @@
 import { EditorManager } from './editor-manager';
 import { TextEditor } from './editor';
 import { injectable, inject } from 'inversify';
+import URI from '@theia/core/lib/common/uri';
 import { StatusBarAlignment, StatusBar } from '@theia/core/lib/browser/status-bar/status-bar';
 import { FrontendApplicationContribution } from '@theia/core/lib/browser';
 import { Languages } from '@theia/languages/lib/browser';
+import { ContextKeyService } from '@theia/core/lib/browser/context-key-service';
 import { DisposableCollection } from '@theia/core';
 import { EditorCommands } from './editor-command';
 
@@ -30,9 +32,39 @@ export class EditorContribution implements FrontendApplicationContribution {
     @inject(EditorManager) protected readonly editorManager: EditorManager;
     @inject(Languages) protected readonly languages: Languages;
 
+    @inject(ContextKeyService)
+    protected readonly contextKeyService: ContextKeyService;
+
     onStart(): void {
+        const resourceSchemeKey = this.contextKeyService.createKey<string>('resourceScheme', undefined);
+        const resourceFileName = this.contextKeyService.createKey<string>('resourceFilename', undefined);
+        const resourceExtname = this.contextKeyService.createKey<string>('resourceExtname', undefined);
+        const resourceLangId = this.contextKeyService.createKey<string>('resourceLangId', undefined);
+        const updateContextKeys = () => {
+            const editor = this.editorManager.currentEditor;
+            const resourceUri = editor && editor.getResourceUri();
+            resourceSchemeKey.set(resourceUri && resourceUri.scheme);
+            resourceFileName.set(resourceUri && resourceUri.path.base);
+            resourceExtname.set(resourceUri && resourceUri.path.ext);
+            resourceLangId.set(this.getLanguageId(resourceUri));
+        };
+        updateContextKeys();
+        this.editorManager.onCurrentEditorChanged(updateContextKeys);
+
         this.updateStatusBar();
         this.editorManager.onCurrentEditorChanged(() => this.updateStatusBar());
+    }
+
+    protected getLanguageId(uri: URI | undefined): string | undefined {
+        const { languages } = this.languages;
+        if (uri && languages) {
+            for (const language of languages) {
+                if (language.extensions.has(uri.path.ext)) {
+                    return language.id;
+                }
+            }
+        }
+        return undefined;
     }
 
     protected readonly toDisposeOnCurrentEditorChanged = new DisposableCollection();

Devpodio/devpodio

Signed-off-by: Uni Sayo <unibtc@gmail.com>

packages/editor/src/browser/editor-contribution.ts
@@ -17,9 +17,11 @@
 import { EditorManager } from './editor-manager';
 import { TextEditor } from './editor';
 import { injectable, inject } from 'inversify';
+import URI from '@devpodio/core/lib/common/uri';
 import { StatusBarAlignment, StatusBar } from '@devpodio/core/lib/browser/status-bar/status-bar';
 import { FrontendApplicationContribution } from '@devpodio/core/lib/browser';
 import { Languages } from '@devpodio/languages/lib/browser';
+import { ContextKeyService } from '@devpodio/core/lib/browser/context-key-service';
 import { DisposableCollection } from '@devpodio/core';
 import { EditorCommands } from './editor-command';
 
@@ -30,9 +32,39 @@ export class EditorContribution implements FrontendApplicationContribution {
     @inject(EditorManager) protected readonly editorManager: EditorManager;
     @inject(Languages) protected readonly languages: Languages;
 
+    @inject(ContextKeyService)
+    protected readonly contextKeyService: ContextKeyService;
+
     onStart(): void {
+        const resourceSchemeKey = this.contextKeyService.createKey<string>('resourceScheme', undefined);
+        const resourceFileName = this.contextKeyService.createKey<string>('resourceFilename', undefined);
+        const resourceExtname = this.contextKeyService.createKey<string>('resourceExtname', undefined);
+        const resourceLangId = this.contextKeyService.createKey<string>('resourceLangId', undefined);
+        const updateContextKeys = () => {
+            const editor = this.editorManager.currentEditor;
+            const resourceUri = editor && editor.getResourceUri();
+            resourceSchemeKey.set(resourceUri && resourceUri.scheme);
+            resourceFileName.set(resourceUri && resourceUri.path.base);
+            resourceExtname.set(resourceUri && resourceUri.path.ext);
+            resourceLangId.set(this.getLanguageId(resourceUri));
+        };
+        updateContextKeys();
+        this.editorManager.onCurrentEditorChanged(updateContextKeys);
+
         this.updateStatusBar();
         this.editorManager.onCurrentEditorChanged(() => this.updateStatusBar());
+    }
+
+    protected getLanguageId(uri: URI | undefined): string | undefined {
+        const { languages } = this.languages;
+        if (uri && languages) {
+            for (const language of languages) {
+                if (language.extensions.has(uri.path.ext)) {
+                    return language.id;
+                }
+            }
+        }
+        return undefined;
     }
 
     protected readonly toDisposeOnCurrentEditorChanged = new DisposableCollection();