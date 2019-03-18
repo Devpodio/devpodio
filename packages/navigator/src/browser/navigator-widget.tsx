pablodonayre/theia

support explorer when contexts
Signed-off-by: Anton Kosyakov <anton.kosyakov@typefox.io>

packages/navigator/src/browser/navigator-widget.tsx
@@ -24,7 +24,7 @@ import {
     TreeProps, TreeModel, TreeNode,
     SelectableTreeNode, CompositeTreeNode
 } from '@theia/core/lib/browser';
-import { FileTreeWidget, FileNode } from '@theia/filesystem/lib/browser';
+import { FileTreeWidget, FileNode, DirNode } from '@theia/filesystem/lib/browser';
 import { WorkspaceService, WorkspaceCommands } from '@theia/workspace/lib/browser';
 import { ApplicationShell } from '@theia/core/lib/browser/shell/application-shell';
 import { WorkspaceNode } from './navigator-tree';
@@ -32,6 +32,7 @@ import { FileNavigatorModel } from './navigator-model';
 import { FileSystem } from '@theia/filesystem/lib/common/filesystem';
 import { isOSX, environment } from '@theia/core';
 import * as React from 'react';
+import { NavigatorContextKeyService } from './navigator-context-key-service';
 
 export const FILE_NAVIGATOR_ID = 'files';
 export const LABEL = 'Files';
@@ -41,6 +42,9 @@ export const CLASS = 'theia-Files';
 export class FileNavigatorWidget extends FileTreeWidget {
 
     @inject(CorePreferences) protected readonly corePreferences: CorePreferences;
+
+    @inject(NavigatorContextKeyService)
+    protected readonly contextKeyService: NavigatorContextKeyService;
 
     constructor(
         @inject(TreeProps) readonly props: TreeProps,
@@ -64,11 +68,13 @@ export class FileNavigatorWidget extends FileTreeWidget {
     @postConstruct()
     protected init(): void {
         super.init();
+        this.updateSelectionContextKeys();
         this.toDispose.pushAll([
             this.model.onSelectionChanged(selection => {
                 if (this.shell.activeWidget === this) {
                     this.selectionService.selection = selection;
                 }
+                this.updateSelectionContextKeys();
             }),
             this.model.onExpansionChanged(node => {
                 if (node.expanded && node.children.length === 1) {
@@ -217,6 +223,20 @@ export class FileNavigatorWidget extends FileTreeWidget {
             this.model.previewNode(node);
         }
         super.handleClickEvent(node, event);
+    }
+
+    protected onAfterShow(msg: Message): void {
+        super.onAfterShow(msg);
+        this.contextKeyService.explorerViewletVisible.set(true);
+    }
+
+    protected onAfterHide(msg: Message): void {
+        super.onAfterHide(msg);
+        this.contextKeyService.explorerViewletVisible.set(false);
+    }
+
+    protected updateSelectionContextKeys(): void {
+        this.contextKeyService.explorerResourceIsFolder.set(DirNode.is(this.model.selectedNodes[0]));
     }
 
 }

Devpodio/devpodio

packages/navigator/src/browser/navigator-widget.tsx
@@ -24,7 +24,7 @@ import {
     TreeProps, TreeModel, TreeNode,
     SelectableTreeNode, CompositeTreeNode
 } from '@devpodio/core/lib/browser';
-import { FileTreeWidget, FileNode } from '@devpodio/filesystem/lib/browser';
+import { FileTreeWidget, FileNode, DirNode } from '@devpodio/filesystem/lib/browser';
 import { WorkspaceService, WorkspaceCommands } from '@devpodio/workspace/lib/browser';
 import { ApplicationShell } from '@devpodio/core/lib/browser/shell/application-shell';
 import { WorkspaceNode } from './navigator-tree';
@@ -32,6 +32,7 @@ import { FileNavigatorModel } from './navigator-model';
 import { FileSystem } from '@devpodio/filesystem/lib/common/filesystem';
 import { isOSX, environment } from '@devpodio/core';
 import * as React from 'react';
+import { NavigatorContextKeyService } from './navigator-context-key-service';
 
 export const FILE_NAVIGATOR_ID = 'files';
 export const LABEL = 'Files';
@@ -41,6 +42,9 @@ export const CLASS = 'theia-Files';
 export class FileNavigatorWidget extends FileTreeWidget {
 
     @inject(CorePreferences) protected readonly corePreferences: CorePreferences;
+
+    @inject(NavigatorContextKeyService)
+    protected readonly contextKeyService: NavigatorContextKeyService;
 
     constructor(
         @inject(TreeProps) readonly props: TreeProps,
@@ -64,11 +68,13 @@ export class FileNavigatorWidget extends FileTreeWidget {
     @postConstruct()
     protected init(): void {
         super.init();
+        this.updateSelectionContextKeys();
         this.toDispose.pushAll([
             this.model.onSelectionChanged(selection => {
                 if (this.shell.activeWidget === this) {
                     this.selectionService.selection = selection;
                 }
+                this.updateSelectionContextKeys();
             }),
             this.model.onExpansionChanged(node => {
                 if (node.expanded && node.children.length === 1) {
@@ -217,6 +223,20 @@ export class FileNavigatorWidget extends FileTreeWidget {
             this.model.previewNode(node);
         }
         super.handleClickEvent(node, event);
+    }
+
+    protected onAfterShow(msg: Message): void {
+        super.onAfterShow(msg);
+        this.contextKeyService.explorerViewletVisible.set(true);
+    }
+
+    protected onAfterHide(msg: Message): void {
+        super.onAfterHide(msg);
+        this.contextKeyService.explorerViewletVisible.set(false);
+    }
+
+    protected updateSelectionContextKeys(): void {
+        this.contextKeyService.explorerResourceIsFolder.set(DirNode.is(this.model.selectedNodes[0]));
     }
 
 }