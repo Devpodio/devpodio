pablodonayre/theia

support debug mode when contexts
Signed-off-by: Anton Kosyakov <anton.kosyakov@typefox.io>

packages/debug/src/browser/debug-configuration-manager.ts
@@ -26,12 +26,14 @@ import URI from '@theia/core/lib/common/uri';
 import { Event, Emitter, ResourceProvider } from '@theia/core';
 import { EditorManager, EditorWidget } from '@theia/editor/lib/browser';
 import { MonacoEditor } from '@theia/monaco/lib/browser/monaco-editor';
-import { QuickPickService, StorageService } from '@theia/core/lib/browser';
+import { StorageService } from '@theia/core/lib/browser';
+import { QuickPickService } from '@theia/core/lib/common/quick-pick-service';
 import { WorkspaceService } from '@theia/workspace/lib/browser/workspace-service';
 import { DebugConfiguration } from '../common/debug-configuration';
 import { DebugConfigurationModel } from './debug-configuration-model';
 import { DebugSessionOptions } from './debug-session-options';
 import { DebugService } from '../common/debug-service';
+import { ContextKey, ContextKeyService } from '@theia/core/lib/browser/context-key-service';
 
 @injectable()
 export class DebugConfigurationManager {
@@ -47,12 +49,18 @@ export class DebugConfigurationManager {
     @inject(QuickPickService)
     protected readonly quickPick: QuickPickService;
 
+    @inject(ContextKeyService)
+    protected readonly contextKeyService: ContextKeyService;
+
     protected readonly onDidChangeEmitter = new Emitter<void>();
     readonly onDidChange: Event<void> = this.onDidChangeEmitter.event;
+
+    protected debugConfigurationTypeKey: ContextKey<string>;
 
     protected initialized: Promise<void>;
     @postConstruct()
     protected async init(): Promise<void> {
+        this.debugConfigurationTypeKey = this.contextKeyService.createKey<string>('debugConfigurationType', undefined);
         this.initialized = this.updateModels();
         this.workspaceService.onWorkspaceChanged(() => this.updateModels());
     }
@@ -137,6 +145,7 @@ export class DebugConfigurationManager {
                 }
             }
         }
+        this.debugConfigurationTypeKey.set(this.current && this.current.configuration.type);
         this.onDidChangeEmitter.fire(undefined);
     }
     find(name: string, workspaceFolderUri: string | undefined): DebugSessionOptions | undefined {

packages/debug/src/browser/debug-frontend-module.ts
@@ -31,6 +31,7 @@ import {
     DebugSessionContributionRegistryImpl
 } from './debug-session-contribution';
 import { bindContributionProvider, ResourceResolver } from '@theia/core';
+import { ContextKeyService } from '@theia/core/lib/browser/context-key-service';
 import { DebugFrontendApplicationContribution } from './debug-frontend-application-contribution';
 import { DebugConsoleContribution } from './console/debug-console-contribution';
 import { BreakpointManager } from './breakpoint/breakpoint-manager';
@@ -42,8 +43,13 @@ import { DebugEditorModelFactory, DebugEditorModel } from './editor/debug-editor
 import './debug-monaco-contribution';
 import { bindDebugPreferences } from './debug-preferences';
 import { DebugSchemaUpdater } from './debug-schema-updater';
+import { DebugCallStackItemTypeKey } from './debug-call-stack-item-type-key';
 
 export default new ContainerModule((bind: interfaces.Bind) => {
+    bind(DebugCallStackItemTypeKey).toDynamicValue(({ container }) =>
+        container.get(ContextKeyService).createKey('callStackItemType', undefined)
+    ).inSingletonScope();
+
     bindContributionProvider(bind, DebugSessionContribution);
     bind(DebugSessionFactory).to(DefaultDebugSessionFactory).inSingletonScope();
     bind(DebugSessionManager).toSelf().inSingletonScope();

packages/debug/src/browser/debug-session-manager.ts
@@ -20,6 +20,7 @@ import { injectable, inject, postConstruct } from 'inversify';
 import { Emitter, Event, DisposableCollection, MessageService } from '@theia/core';
 import { LabelProvider } from '@theia/core/lib/browser';
 import { EditorManager } from '@theia/editor/lib/browser';
+import { ContextKeyService, ContextKey } from '@theia/core/lib/browser/context-key-service';
 import { DebugError, DebugService } from '../common/debug-service';
 import { DebugState, DebugSession } from './debug-session';
 import { DebugSessionFactory, DebugSessionContributionRegistry } from './debug-session-contribution';
@@ -78,6 +79,7 @@ export class DebugSessionManager {
     protected readonly onDidChangeEmitter = new Emitter<DebugSession | undefined>();
     readonly onDidChange: Event<DebugSession | undefined> = this.onDidChangeEmitter.event;
     protected fireDidChange(current: DebugSession | undefined): void {
+        this.inDebugModeKey.set(this.inDebugMode);
         this.onDidChangeEmitter.fire(current);
     }
 
@@ -105,9 +107,21 @@ export class DebugSessionManager {
     @inject(MessageService)
     protected readonly messageService: MessageService;
 
+    @inject(ContextKeyService)
+    protected readonly contextKeyService: ContextKeyService;
+
+    protected debugTypeKey: ContextKey<string>;
+    protected inDebugModeKey: ContextKey<boolean>;
+
     @postConstruct()
     protected init(): void {
+        this.debugTypeKey = this.contextKeyService.createKey<string>('debugType', undefined);
+        this.inDebugModeKey = this.contextKeyService.createKey<boolean>('inDebugMode', this.inDebugMode);
         this.breakpoints.onDidChangeMarkers(uri => this.fireDidChangeBreakpoints({ uri }));
+    }
+
+    get inDebugMode(): boolean {
+        return this.state > DebugState.Inactive;
     }
 
     async start(options: DebugSessionOptions): Promise<DebugSession | undefined> {
@@ -149,6 +163,7 @@ export class DebugSessionManager {
         const session = sessionFactory.get(sessionId, options);
         this._sessions.set(sessionId, session);
 
+        this.debugTypeKey.set(session.configuration.type);
         this.onDidCreateDebugSessionEmitter.fire(session);
 
         let state = DebugState.Inactive;

Devpodio/devpodio

Signed-off-by: Uni Sayo <unibtc@gmail.com>

packages/debug/src/browser/debug-configuration-manager.ts
@@ -26,12 +26,14 @@ import URI from '@devpodio/core/lib/common/uri';
 import { Event, Emitter, ResourceProvider } from '@devpodio/core';
 import { EditorManager, EditorWidget } from '@devpodio/editor/lib/browser';
 import { MonacoEditor } from '@devpodio/monaco/lib/browser/monaco-editor';
-import { QuickPickService, StorageService } from '@devpodio/core/lib/browser';
+import { StorageService } from '@devpodio/core/lib/browser';
+import { QuickPickService } from '@devpodio/core/lib/common/quick-pick-service';
 import { WorkspaceService } from '@devpodio/workspace/lib/browser/workspace-service';
 import { DebugConfiguration } from '../common/debug-configuration';
 import { DebugConfigurationModel } from './debug-configuration-model';
 import { DebugSessionOptions } from './debug-session-options';
 import { DebugService } from '../common/debug-service';
+import { ContextKey, ContextKeyService } from '@devpodio/core/lib/browser/context-key-service';
 
 @injectable()
 export class DebugConfigurationManager {
@@ -47,12 +49,18 @@ export class DebugConfigurationManager {
     @inject(QuickPickService)
     protected readonly quickPick: QuickPickService;
 
+    @inject(ContextKeyService)
+    protected readonly contextKeyService: ContextKeyService;
+
     protected readonly onDidChangeEmitter = new Emitter<void>();
     readonly onDidChange: Event<void> = this.onDidChangeEmitter.event;
+
+    protected debugConfigurationTypeKey: ContextKey<string>;
 
     protected initialized: Promise<void>;
     @postConstruct()
     protected async init(): Promise<void> {
+        this.debugConfigurationTypeKey = this.contextKeyService.createKey<string>('debugConfigurationType', undefined);
         this.initialized = this.updateModels();
         this.workspaceService.onWorkspaceChanged(() => this.updateModels());
     }
@@ -137,6 +145,7 @@ export class DebugConfigurationManager {
                 }
             }
         }
+        this.debugConfigurationTypeKey.set(this.current && this.current.configuration.type);
         this.onDidChangeEmitter.fire(undefined);
     }
     find(name: string, workspaceFolderUri: string | undefined): DebugSessionOptions | undefined {

packages/debug/src/browser/debug-frontend-module.ts
@@ -31,6 +31,7 @@ import {
     DebugSessionContributionRegistryImpl
 } from './debug-session-contribution';
 import { bindContributionProvider, ResourceResolver } from '@devpodio/core';
+import { ContextKeyService } from '@devpodio/core/lib/browser/context-key-service';
 import { DebugFrontendApplicationContribution } from './debug-frontend-application-contribution';
 import { DebugConsoleContribution } from './console/debug-console-contribution';
 import { BreakpointManager } from './breakpoint/breakpoint-manager';
@@ -42,8 +43,13 @@ import { DebugEditorModelFactory, DebugEditorModel } from './editor/debug-editor
 import './debug-monaco-contribution';
 import { bindDebugPreferences } from './debug-preferences';
 import { DebugSchemaUpdater } from './debug-schema-updater';
+import { DebugCallStackItemTypeKey } from './debug-call-stack-item-type-key';
 
 export default new ContainerModule((bind: interfaces.Bind) => {
+    bind(DebugCallStackItemTypeKey).toDynamicValue(({ container }) =>
+        container.get(ContextKeyService).createKey('callStackItemType', undefined)
+    ).inSingletonScope();
+
     bindContributionProvider(bind, DebugSessionContribution);
     bind(DebugSessionFactory).to(DefaultDebugSessionFactory).inSingletonScope();
     bind(DebugSessionManager).toSelf().inSingletonScope();

packages/debug/src/browser/debug-session-manager.ts
@@ -20,6 +20,7 @@ import { injectable, inject, postConstruct } from 'inversify';
 import { Emitter, Event, DisposableCollection, MessageService } from '@devpodio/core';
 import { LabelProvider } from '@devpodio/core/lib/browser';
 import { EditorManager } from '@devpodio/editor/lib/browser';
+import { ContextKeyService, ContextKey } from '@devpodio/core/lib/browser/context-key-service';
 import { DebugError, DebugService } from '../common/debug-service';
 import { DebugState, DebugSession } from './debug-session';
 import { DebugSessionFactory, DebugSessionContributionRegistry } from './debug-session-contribution';
@@ -78,6 +79,7 @@ export class DebugSessionManager {
     protected readonly onDidChangeEmitter = new Emitter<DebugSession | undefined>();
     readonly onDidChange: Event<DebugSession | undefined> = this.onDidChangeEmitter.event;
     protected fireDidChange(current: DebugSession | undefined): void {
+        this.inDebugModeKey.set(this.inDebugMode);
         this.onDidChangeEmitter.fire(current);
     }
 
@@ -105,9 +107,21 @@ export class DebugSessionManager {
     @inject(MessageService)
     protected readonly messageService: MessageService;
 
+    @inject(ContextKeyService)
+    protected readonly contextKeyService: ContextKeyService;
+
+    protected debugTypeKey: ContextKey<string>;
+    protected inDebugModeKey: ContextKey<boolean>;
+
     @postConstruct()
     protected init(): void {
+        this.debugTypeKey = this.contextKeyService.createKey<string>('debugType', undefined);
+        this.inDebugModeKey = this.contextKeyService.createKey<boolean>('inDebugMode', this.inDebugMode);
         this.breakpoints.onDidChangeMarkers(uri => this.fireDidChangeBreakpoints({ uri }));
+    }
+
+    get inDebugMode(): boolean {
+        return this.state > DebugState.Inactive;
     }
 
     async start(options: DebugSessionOptions): Promise<DebugSession | undefined> {
@@ -149,6 +163,7 @@ export class DebugSessionManager {
         const session = sessionFactory.get(sessionId, options);
         this._sessions.set(sessionId, session);
 
+        this.debugTypeKey.set(session.configuration.type);
         this.onDidCreateDebugSessionEmitter.fire(session);
 
         let state = DebugState.Inactive;