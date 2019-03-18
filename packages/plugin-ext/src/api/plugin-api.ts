zgqing/theia

[plugin] fix #4032: ensure uniqueness of status bar entries
Signed-off-by: Anton Kosyakov <anton.kosyakov@typefox.io>

packages/plugin-ext/src/api/plugin-api.ts
@@ -257,14 +257,15 @@ export interface MessageRegistryMain {
 }
 
 export interface StatusBarMessageRegistryMain {
-    $setMessage(text: string | undefined,
+    $setElement(id: string,
+        text: string | undefined,
         priority: number,
         alignment: theia.StatusBarAlignment,
         color: string | undefined,
         tooltip: string | undefined,
-        command: string | undefined): PromiseLike<string>;
+        command: string | undefined): PromiseLike<void>;
     $update(id: string, message: string): void;
-    $dispose(id: string): void;
+    $removeElement(id: string): void;
 }
 
 export interface QuickOpenExt {

packages/plugin-ext/src/plugin/status-bar-message-registry.ts
@@ -13,13 +13,13 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
-import {Disposable, StatusBarAlignment} from './types-impl';
-import {StatusBarItem} from '@theia/plugin';
+import { Disposable, StatusBarAlignment } from './types-impl';
+import { StatusBarItem } from '@theia/plugin';
 import {
     PLUGIN_RPC_CONTEXT as Ext, StatusBarMessageRegistryMain
 } from '../api/plugin-api';
-import {RPCProtocol} from '../api/rpc-protocol';
-import {StatusBarItemImpl} from './status-bar/status-bar-item';
+import { RPCProtocol } from '../api/rpc-protocol';
+import { StatusBarItemImpl } from './status-bar/status-bar-item';
 
 export class StatusBarMessageRegistryExt {
 
@@ -31,14 +31,12 @@ export class StatusBarMessageRegistryExt {
 
     // tslint:disable-next-line:no-any
     setStatusBarMessage(text: string, arg?: number | PromiseLike<any>): Disposable {
-        let id: string;
-        this.proxy.$setMessage(text, 0, 1, undefined, undefined, undefined).then((messageId: string) => {
-            id = messageId;
-        });
-        let handle: NodeJS.Timer;
+        const id = StatusBarItemImpl.nextId();
+        this.proxy.$setElement(id, text, 0, 1, undefined, undefined, undefined);
+        let handle: NodeJS.Timer | undefined;
 
         if (typeof arg === 'number') {
-            handle = setTimeout(() => this.dispose(id), <number>arg);
+            handle = setTimeout(() => this.dispose(id), arg);
         } else if (typeof arg !== 'undefined') {
             arg.then(() => this.dispose(id), () => this.dispose(id));
         }
@@ -52,10 +50,7 @@ export class StatusBarMessageRegistryExt {
     }
 
     private dispose(id: string): void {
-        if (!id) {
-            return;
-        }
-        this.proxy.$dispose(id);
+        this.proxy.$removeElement(id);
     }
 
     createStatusBarItem(alignment?: StatusBarAlignment, priority?: number): StatusBarItem {

packages/plugin-ext/src/plugin/status-bar/status-bar-item.ts
@@ -17,9 +17,12 @@ import * as theia from '@theia/plugin';
 import { ThemeColor, StatusBarAlignment } from '../types-impl';
 import { StatusBarMessageRegistryMain } from '../../api/plugin-api';
 import { VS_COLORS } from './vscolor-const';
+import { UUID } from '@phosphor/coreutils/lib/uuid';
 
 export class StatusBarItemImpl implements theia.StatusBarItem {
-    private _messageId: string;
+
+    private readonly id = StatusBarItemImpl.nextId();
+
     private _alignment: StatusBarAlignment;
     private _priority: number;
 
@@ -94,9 +97,7 @@ export class StatusBarItemImpl implements theia.StatusBarItem {
         if (this._timeoutHandle) {
             clearTimeout(this._timeoutHandle);
         }
-        if (this._messageId) {
-            this._proxy.$dispose(this._messageId);
-        }
+        this._proxy.$removeElement(this.id);
         this._isVisible = false;
     }
 
@@ -104,28 +105,20 @@ export class StatusBarItemImpl implements theia.StatusBarItem {
         if (!this._isVisible) {
             return;
         }
-
-        if (this._messageId) {
-            this._proxy.$dispose(this._messageId);
-        }
-
         if (this._timeoutHandle) {
             clearTimeout(this._timeoutHandle);
         }
-
         // Defer the update so that multiple changes to setters don't cause a redraw each
         this._timeoutHandle = setTimeout(() => {
             this._timeoutHandle = undefined;
 
             // Set to status bar
-            this._proxy.$setMessage(this.text,
+            this._proxy.$setElement(this.id, this.text,
                 this.priority,
                 this.alignment,
                 this.getColor(),
                 this.tooltip,
-                this.command).then((id: string) => {
-                    this._messageId = id;
-                });
+                this.command);
         }, 0);
     }
 
@@ -139,5 +132,9 @@ export class StatusBarItemImpl implements theia.StatusBarItem {
 
     public dispose(): void {
         this.hide();
+    }
+
+    static nextId(): string {
+        return 'plugin-status-bar-item:' + UUID.uuid4();
     }
 }

packages/plugin-ext/src/plugin/statusBar.ts
@@ -19,6 +19,7 @@ import { CancellationToken, Progress, ProgressOptions } from '@theia/plugin';
 import { RPCProtocol } from '../api/rpc-protocol';
 import { Event, Emitter } from '@theia/core/lib/common/event';
 import { Disposable, DisposableCollection } from '@theia/core/lib/common/disposable';
+import { StatusBarItemImpl } from './status-bar/status-bar-item';
 
 export class StatusBarExtImpl implements StatusBarExt {
     private readonly proxy: StatusBarMessageRegistryMain;
@@ -30,10 +31,11 @@ export class StatusBarExtImpl implements StatusBarExt {
     ): Promise<R> {
         const message = options.title ? '$(refresh~spin) ' + options.title : '';
         const token = new CancellationTokenImpl(this.onCancel);
-        const id = await this.proxy.$setMessage(message, 1, 1, undefined, undefined, undefined);
+        const id = StatusBarItemImpl.nextId();
+        await this.proxy.$setElement(id, message, 1, 1, undefined, undefined, undefined);
         const promise = task(new ProgressCallback(id, message, this.proxy), token);
         await promise;
-        this.proxy.$dispose(id);
+        this.proxy.$removeElement(id);
         token.dispose();
         return promise;
     }

Devpodio/devpodio

packages/plugin-ext/src/api/plugin-api.ts
@@ -257,12 +257,13 @@ export interface MessageRegistryMain {
 }
 
 export interface StatusBarMessageRegistryMain {
-    $setMessage(text: string | undefined,
+    $setMessage(id: string,
+        text: string | undefined,
         priority: number,
         alignment: theia.StatusBarAlignment,
         color: string | undefined,
         tooltip: string | undefined,
-        command: string | undefined): PromiseLike<string>;
+        command: string | undefined): PromiseLike<void>;
     $update(id: string, message: string): void;
     $dispose(id: string): void;
 }

packages/plugin-ext/src/plugin/status-bar-message-registry.ts
@@ -13,13 +13,13 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
-import {Disposable, StatusBarAlignment} from './types-impl';
-import {StatusBarItem} from '@devpodio/plugin';
+import { Disposable, StatusBarAlignment } from './types-impl';
+import { StatusBarItem } from '@devpodio/plugin';
 import {
     PLUGIN_RPC_CONTEXT as Ext, StatusBarMessageRegistryMain
 } from '../api/plugin-api';
-import {RPCProtocol} from '../api/rpc-protocol';
-import {StatusBarItemImpl} from './status-bar/status-bar-item';
+import { RPCProtocol } from '../api/rpc-protocol';
+import { StatusBarItemImpl } from './status-bar/status-bar-item';
 
 export class StatusBarMessageRegistryExt {
 
@@ -31,14 +31,12 @@ export class StatusBarMessageRegistryExt {
 
     // tslint:disable-next-line:no-any
     setStatusBarMessage(text: string, arg?: number | PromiseLike<any>): Disposable {
-        let id: string;
-        this.proxy.$setMessage(text, 0, 1, undefined, undefined, undefined).then((messageId: string) => {
-            id = messageId;
-        });
-        let handle: NodeJS.Timer;
+        const id = StatusBarItemImpl.nextId();
+        this.proxy.$setMessage(id, text, 0, 1, undefined, undefined, undefined);
+        let handle: NodeJS.Timer | undefined;
 
         if (typeof arg === 'number') {
-            handle = setTimeout(() => this.dispose(id), <number>arg);
+            handle = setTimeout(() => this.dispose(id), arg);
         } else if (typeof arg !== 'undefined') {
             arg.then(() => this.dispose(id), () => this.dispose(id));
         }
@@ -52,9 +50,6 @@ export class StatusBarMessageRegistryExt {
     }
 
     private dispose(id: string): void {
-        if (!id) {
-            return;
-        }
         this.proxy.$dispose(id);
     }
 

packages/plugin-ext/src/plugin/status-bar/status-bar-item.ts
@@ -17,9 +17,12 @@ import * as theia from '@devpodio/plugin';
 import { ThemeColor, StatusBarAlignment } from '../types-impl';
 import { StatusBarMessageRegistryMain } from '../../api/plugin-api';
 import { VS_COLORS } from './vscolor-const';
+import { UUID } from '@phosphor/coreutils/lib/uuid';
 
 export class StatusBarItemImpl implements theia.StatusBarItem {
-    private _messageId: string;
+
+    private readonly id = StatusBarItemImpl.nextId();
+
     private _alignment: StatusBarAlignment;
     private _priority: number;
 
@@ -94,9 +97,7 @@ export class StatusBarItemImpl implements theia.StatusBarItem {
         if (this._timeoutHandle) {
             clearTimeout(this._timeoutHandle);
         }
-        if (this._messageId) {
-            this._proxy.$dispose(this._messageId);
-        }
+        this._proxy.$dispose(this.id);
         this._isVisible = false;
     }
 
@@ -104,28 +105,20 @@ export class StatusBarItemImpl implements theia.StatusBarItem {
         if (!this._isVisible) {
             return;
         }
-
-        if (this._messageId) {
-            this._proxy.$dispose(this._messageId);
-        }
-
         if (this._timeoutHandle) {
             clearTimeout(this._timeoutHandle);
         }
-
         // Defer the update so that multiple changes to setters don't cause a redraw each
         this._timeoutHandle = setTimeout(() => {
             this._timeoutHandle = undefined;
 
             // Set to status bar
-            this._proxy.$setMessage(this.text,
+            this._proxy.$setMessage(this.id, this.text,
                 this.priority,
                 this.alignment,
                 this.getColor(),
                 this.tooltip,
-                this.command).then((id: string) => {
-                    this._messageId = id;
-                });
+                this.command);
         }, 0);
     }
 
@@ -140,4 +133,9 @@ export class StatusBarItemImpl implements theia.StatusBarItem {
     public dispose(): void {
         this.hide();
     }
+
+    static nextId(): string {
+        return StatusBarItemImpl.ID_PREFIX + ':' + UUID.uuid4();
+    }
+    static ID_PREFIX = 'plugin-status-bar-item';
 }

packages/plugin-ext/src/plugin/statusBar.ts
@@ -19,6 +19,7 @@ import { CancellationToken, Progress, ProgressOptions } from '@devpodio/plugin';
 import { RPCProtocol } from '../api/rpc-protocol';
 import { Event, Emitter } from '@devpodio/core/lib/common/event';
 import { Disposable, DisposableCollection } from '@devpodio/core/lib/common/disposable';
+import { StatusBarItemImpl } from './status-bar/status-bar-item';
 
 export class StatusBarExtImpl implements StatusBarExt {
     private readonly proxy: StatusBarMessageRegistryMain;
@@ -30,7 +31,8 @@ export class StatusBarExtImpl implements StatusBarExt {
     ): Promise<R> {
         const message = options.title ? '$(refresh~spin) ' + options.title : '';
         const token = new CancellationTokenImpl(this.onCancel);
-        const id = await this.proxy.$setMessage(message, 1, 1, undefined, undefined, undefined);
+        const id = StatusBarItemImpl.nextId();
+        await this.proxy.$setMessage(id, message, 1, 1, undefined, undefined, undefined);
         const promise = task(new ProgressCallback(id, message, this.proxy), token);
         await promise;
         this.proxy.$dispose(id);