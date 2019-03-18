eclipse-theia/theia

fix #4073: support configuration settings when contexts
Signed-off-by: Anton Kosyakov <anton.kosyakov@typefox.io>

packages/monaco/src/browser/monaco-frontend-module.ts
@@ -18,9 +18,9 @@ import '../../src/browser/style/index.css';
 import '../../src/browser/style/symbol-sprite.svg';
 import '../../src/browser/style/symbol-icons.css';
 
-import { ContainerModule, decorate, injectable } from 'inversify';
+import { ContainerModule, decorate, injectable, interfaces } from 'inversify';
 import { MenuContribution, CommandContribution } from '@theia/core/lib/common';
-import { QuickOpenService, FrontendApplicationContribution, KeybindingContribution } from '@theia/core/lib/browser';
+import { QuickOpenService, FrontendApplicationContribution, KeybindingContribution, PreferenceServiceImpl } from '@theia/core/lib/browser';
 import { Languages, Workspace } from '@theia/languages/lib/browser';
 import { TextEditorProvider, DiffNavigatorProvider } from '@theia/editor/lib/browser';
 import { StrictEditorTextFocusContext } from '@theia/editor/lib/browser/editor-keybinding-contexts';
@@ -53,6 +53,8 @@ import { MonacoSnippetSuggestProvider } from './monaco-snippet-suggest-provider'
 import { ContextKeyService } from '@theia/core/lib/browser/context-key-service';
 import { MonacoContextKeyService } from './monaco-context-key-service';
 
+const deepmerge: (args: object[]) => object = require('deepmerge').default.all;
+
 decorate(injectable(), MonacoToProtocolConverter);
 decorate(injectable(), ProtocolToMonacoConverter);
 decorate(injectable(), monaco.contextKeyService.ContextKeyService);
@@ -74,10 +76,12 @@ export default new ContainerModule((bind, unbind, isBound, rebind) => {
     bind(MonacoWorkspace).toSelf().inSingletonScope();
     bind(Workspace).toService(MonacoWorkspace);
 
-    // TODO: https://github.com/theia-ide/theia/issues/4073
-    const configurationService = monaco.services.StaticServices.configurationService.get();
-    const contextKeyService = new monaco.contextKeyService.ContextKeyService(configurationService);
-    bind(monaco.contextKeyService.ContextKeyService).toConstantValue(contextKeyService);
+    bind(MonacoConfigurationService).toDynamicValue(({ container }) =>
+        createMonacoConfigurationService(container)
+    ).inSingletonScope();
+    bind(monaco.contextKeyService.ContextKeyService).toDynamicValue(({ container }) =>
+        new monaco.contextKeyService.ContextKeyService(container.get(MonacoConfigurationService))
+    ).inSingletonScope();
     bind(MonacoBulkEditService).toSelf().inSingletonScope();
     bind(MonacoEditorService).toSelf().inSingletonScope();
     bind(MonacoTextModelService).toSelf().inSingletonScope();
@@ -116,3 +120,33 @@ export default new ContainerModule((bind, unbind, isBound, rebind) => {
     bind(MonacoOutlineDecorator).toSelf().inSingletonScope();
     bind(OutlineTreeDecorator).toService(MonacoOutlineDecorator);
 });
+
+export const MonacoConfigurationService = Symbol('MonacoConfigurationService');
+export function createMonacoConfigurationService(container: interfaces.Container): monaco.services.IConfigurationService {
+    const configurations = container.get(MonacoConfigurations);
+    const preferences = container.get(PreferenceServiceImpl);
+    const service = monaco.services.StaticServices.configurationService.get();
+    const _configuration = service._configuration;
+
+    const getValue = _configuration.getValue.bind(_configuration);
+    _configuration.getValue = (section, overrides, workspace) => {
+        const preferenceConfig = configurations.getConfiguration();
+        if (section) {
+            const value = preferenceConfig.get(section);
+            return value !== undefined ? value : getValue(section, overrides, workspace);
+        }
+        const simpleConfig = getValue(section, overrides, workspace);
+        if (typeof simpleConfig === 'object') {
+            return deepmerge([{}, simpleConfig, preferenceConfig.toJSON()]);
+        }
+        return preferenceConfig.toJSON();
+    };
+
+    preferences.onPreferencesChanged(changes => {
+        const event = new monaco.services.ConfigurationChangeEvent();
+        event.change(Object.keys(changes));
+        service._onDidChangeConfiguration.fire(event);
+    });
+
+    return service;
+}

Devpodio/devpodio

packages/monaco/src/browser/monaco-frontend-module.ts
@@ -18,9 +18,9 @@ import '../../src/browser/style/index.css';
 import '../../src/browser/style/symbol-sprite.svg';
 import '../../src/browser/style/symbol-icons.css';
 
-import { ContainerModule, decorate, injectable } from 'inversify';
+import { ContainerModule, decorate, injectable, interfaces } from 'inversify';
 import { MenuContribution, CommandContribution } from '@devpodio/core/lib/common';
-import { QuickOpenService, FrontendApplicationContribution, KeybindingContribution } from '@devpodio/core/lib/browser';
+import { QuickOpenService, FrontendApplicationContribution, KeybindingContribution, PreferenceServiceImpl } from '@devpodio/core/lib/browser';
 import { Languages, Workspace } from '@devpodio/languages/lib/browser';
 import { TextEditorProvider, DiffNavigatorProvider } from '@devpodio/editor/lib/browser';
 import { StrictEditorTextFocusContext } from '@devpodio/editor/lib/browser/editor-keybinding-contexts';
@@ -53,6 +53,8 @@ import { MonacoSnippetSuggestProvider } from './monaco-snippet-suggest-provider'
 import { ContextKeyService } from '@theia/core/lib/browser/context-key-service';
 import { MonacoContextKeyService } from './monaco-context-key-service';
 
+const deepmerge: (args: object[]) => object = require('deepmerge').default.all;
+
 decorate(injectable(), MonacoToProtocolConverter);
 decorate(injectable(), ProtocolToMonacoConverter);
 decorate(injectable(), monaco.contextKeyService.ContextKeyService);
@@ -74,10 +76,12 @@ export default new ContainerModule((bind, unbind, isBound, rebind) => {
     bind(MonacoWorkspace).toSelf().inSingletonScope();
     bind(Workspace).toService(MonacoWorkspace);
 
-    // TODO: https://github.com/theia-ide/theia/issues/4073
-    const configurationService = monaco.services.StaticServices.configurationService.get();
-    const contextKeyService = new monaco.contextKeyService.ContextKeyService(configurationService);
-    bind(monaco.contextKeyService.ContextKeyService).toConstantValue(contextKeyService);
+    bind(MonacoConfigurationService).toDynamicValue(({ container }) =>
+        createMonacoConfigurationService(container)
+    ).inSingletonScope();
+    bind(monaco.contextKeyService.ContextKeyService).toDynamicValue(({ container }) =>
+        new monaco.contextKeyService.ContextKeyService(container.get(MonacoConfigurationService))
+    ).inSingletonScope();
     bind(MonacoBulkEditService).toSelf().inSingletonScope();
     bind(MonacoEditorService).toSelf().inSingletonScope();
     bind(MonacoTextModelService).toSelf().inSingletonScope();
@@ -116,3 +120,33 @@ export default new ContainerModule((bind, unbind, isBound, rebind) => {
     bind(MonacoOutlineDecorator).toSelf().inSingletonScope();
     bind(OutlineTreeDecorator).toService(MonacoOutlineDecorator);
 });
+
+export const MonacoConfigurationService = Symbol('MonacoConfigurationService');
+export function createMonacoConfigurationService(container: interfaces.Container): monaco.services.IConfigurationService {
+    const configurations = container.get(MonacoConfigurations);
+    const preferences = container.get(PreferenceServiceImpl);
+    const service = monaco.services.StaticServices.configurationService.get();
+    const _configuration = service._configuration;
+
+    const getValue = _configuration.getValue.bind(_configuration);
+    _configuration.getValue = (section, overrides, workspace) => {
+        const preferenceConfig = configurations.getConfiguration();
+        if (section) {
+            const value = preferenceConfig.get(section);
+            return value !== undefined ? value : getValue(section, overrides, workspace);
+        }
+        const simpleConfig = getValue(section, overrides, workspace);
+        if (typeof simpleConfig === 'object') {
+            return deepmerge([{}, simpleConfig, preferenceConfig.toJSON()]);
+        }
+        return preferenceConfig.toJSON();
+    };
+
+    preferences.onPreferencesChanged(changes => {
+        const event = new monaco.services.ConfigurationChangeEvent();
+        event.change(Object.keys(changes));
+        service._onDidChangeConfiguration.fire(event);
+    });
+
+    return service;
+}