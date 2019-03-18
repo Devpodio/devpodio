pablodonayre/theia

support workspace when contexts
Signed-off-by: Anton Kosyakov <anton.kosyakov@typefox.io>

packages/workspace/src/browser/workspace-frontend-contribution.ts
@@ -14,12 +14,13 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-import { injectable, inject } from 'inversify';
+import { injectable, inject, postConstruct } from 'inversify';
 import { CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry } from '@theia/core/lib/common';
 import { isOSX, environment, OS } from '@theia/core';
 import { open, OpenerService, CommonMenus, StorageService, LabelProvider, ConfirmDialog, KeybindingRegistry, KeybindingContribution } from '@theia/core/lib/browser';
 import { FileDialogService, OpenFileDialogProps, FileDialogTreeFilters } from '@theia/filesystem/lib/browser';
 import { FileSystem } from '@theia/filesystem/lib/common';
+import { ContextKeyService } from '@theia/core/lib/browser/context-key-service';
 import { WorkspaceService } from './workspace-service';
 import { THEIA_EXT, VSCODE_EXT } from '../common';
 import { WorkspaceCommands } from './workspace-commands';
@@ -38,6 +39,21 @@ export class WorkspaceFrontendContribution implements CommandContribution, Keybi
     @inject(QuickOpenWorkspace) protected readonly quickOpenWorkspace: QuickOpenWorkspace;
     @inject(FileDialogService) protected readonly fileDialogService: FileDialogService;
     @inject(WorkspacePreferences) protected preferences: WorkspacePreferences;
+
+    @inject(ContextKeyService)
+    protected readonly contextKeyService: ContextKeyService;
+
+    @postConstruct()
+    protected init(): void {
+        this.initWorkspaceContextKeys();
+    }
+
+    protected initWorkspaceContextKeys(): void {
+        const workspaceFolderCountKey = this.contextKeyService.createKey<number>('workspaceFolderCount', 0);
+        const updateWorkspaceFolderCountKey = () => workspaceFolderCountKey.set(this.workspaceService.tryGetRoots().length);
+        updateWorkspaceFolderCountKey();
+        this.workspaceService.onWorkspaceChanged(updateWorkspaceFolderCountKey);
+    }
 
     registerCommands(commands: CommandRegistry): void {
         // Not visible/enabled on Windows/Linux in electron.

Devpodio/devpodio

packages/workspace/src/browser/workspace-frontend-contribution.ts
@@ -14,12 +14,13 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-import { injectable, inject } from 'inversify';
+import { injectable, inject, postConstruct } from 'inversify';
 import { CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry } from '@devpodio/core/lib/common';
 import { isOSX, environment, OS } from '@devpodio/core';
 import { open, OpenerService, CommonMenus, StorageService, LabelProvider, ConfirmDialog, KeybindingRegistry, KeybindingContribution } from '@devpodio/core/lib/browser';
 import { FileDialogService, OpenFileDialogProps, FileDialogTreeFilters } from '@devpodio/filesystem/lib/browser';
 import { FileSystem } from '@devpodio/filesystem/lib/common';
+import { ContextKeyService } from '@devpodio/core/lib/browser/context-key-service';
 import { WorkspaceService } from './workspace-service';
 import { THEIA_EXT, VSCODE_EXT } from '../common';
 import { WorkspaceCommands } from './workspace-commands';
@@ -38,6 +39,21 @@ export class WorkspaceFrontendContribution implements CommandContribution, Keybi
     @inject(QuickOpenWorkspace) protected readonly quickOpenWorkspace: QuickOpenWorkspace;
     @inject(FileDialogService) protected readonly fileDialogService: FileDialogService;
     @inject(WorkspacePreferences) protected preferences: WorkspacePreferences;
+
+    @inject(ContextKeyService)
+    protected readonly contextKeyService: ContextKeyService;
+
+    @postConstruct()
+    protected init(): void {
+        this.initWorkspaceContextKeys();
+    }
+
+    protected initWorkspaceContextKeys(): void {
+        const workspaceFolderCountKey = this.contextKeyService.createKey<number>('workspaceFolderCount', 0);
+        const updateWorkspaceFolderCountKey = () => workspaceFolderCountKey.set(this.workspaceService.tryGetRoots().length);
+        updateWorkspaceFolderCountKey();
+        this.workspaceService.onWorkspaceChanged(updateWorkspaceFolderCountKey);
+    }
 
     registerCommands(commands: CommandRegistry): void {
         // Not visible/enabled on Windows/Linux in electron.